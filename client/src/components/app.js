71e68e0e51b8a6c3257ffdf6f4c59862e22d19c1
Yashraj Chavda <yashrajpchavda@gmail.com>
2019-10-12 21:38:22 +0530

    master: updated the build with no prerender

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -7,26 +7,30 @@ import Header from './header';
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 
-export default class App extends Component {
+const App = () => {
+
+	let currentUrl;
 	
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
+	const handleRoute = e => {
+		currentUrl = e.url;
 	};
 
-	render() {
-		return (
-			<div id="app">
-				<Header />
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
-				</Router>
-			</div>
-		);
-	}
+	
+	return (
+		<div id="app">
+			<Header />
+			<Router onChange={handleRoute}>
+				<Home path="/" />
+				<Profile path="/profile/" user="me" />
+				<Profile path="/profile/:user" />
+			</Router>
+		</div>
+	);
+	
 }
+
+export default App;
