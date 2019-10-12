388c78ffa995c160df857c540601011ca63b66c6
Yashraj Chavda <yashrajpchavda@gmail.com>
2019-10-12 21:49:31 +0530

    master: added a simple graphql query in preact app

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,11 +1,30 @@
 import { createElement } from 'preact';
 import style from './style';
+import gql from 'graphql-tag';
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home1234</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+import { useQuery } from '@apollo/react-hooks';
+
+const SAY_HI = gql`
+	{
+		sayHi
+	}
+`;
+
+const Home = () => {
+	const { loading, data } = useQuery(SAY_HI);
+
+	if(data) {
+		console.log('data is', data);
+	}
+	return (
+		loading ? 
+			<h1>Loading...</h1> :
+			(<div class={style.home}>
+				<h1>{data.sayHi}</h1>
+				<p>This is the Home component.</p>
+			</div>)
+		
+	)
+};
 
 export default Home;
