c37e70e14f8a5cce9dd91939b7e62a06da40a43e
Yashraj Chavda <yashrajpchavda@gmail.com>
2019-10-17 22:12:27 +0530

    master: update home component to include spots

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,17 +1,38 @@
-import { createElement } from 'preact';
 import style from './style';
 import gql from 'graphql-tag';
 
+import { h, Component } from 'preact';
+import Card from 'preact-material-components/Card';
+import LayoutGrid from 'preact-material-components/LayoutGrid';
+import 'preact-material-components/LayoutGrid/style.css';
+import 'preact-material-components/Card/style.css';
+import 'preact-material-components/Button/style.css';
+
 import { useQuery } from '@apollo/react-hooks';
 
-const SAY_HI = gql`
-	{
-		sayHi
+const GET_PARKING_SPOTS = gql`
+	query GetParkingSpots {
+		getParkingSpots{
+			id
+			isOccupied
+			number
+			car {
+				id
+				name
+				plate
+			}
+			user {
+				id
+				username
+				displayName
+				email
+			}
+		}
 	}
 `;
 
 const Home = () => {
-	const { loading, data } = useQuery(SAY_HI);
+	const { loading, data } = useQuery(GET_PARKING_SPOTS);
 
 	if(data) {
 		console.log('data is', data);
@@ -20,8 +41,47 @@ const Home = () => {
 		loading ? 
 			<h1>Loading...</h1> :
 			(<div class={style.home}>
-				<h1>{data.sayHi}</h1>
+				<h1>Loaded</h1>
 				<p>This is the Home component.</p>
+				<div>
+					<LayoutGrid>
+						<LayoutGrid.Inner>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								1
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								2
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								3
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								4
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								5
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								6
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								7
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								8
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								9
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								10
+							</LayoutGrid.Cell>
+							<LayoutGrid.Cell cols="3" phoneCols="1">
+								11
+							</LayoutGrid.Cell>
+						</LayoutGrid.Inner>
+					</LayoutGrid>
+				</div>
 			</div>)
 		
 	)
